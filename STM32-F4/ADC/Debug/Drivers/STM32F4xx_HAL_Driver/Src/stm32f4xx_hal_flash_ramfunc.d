9040135013601e9c2818553a62a0b3c38d48b805
roeetalorbit <roeetalorbit@users.noreply.github.com>
2016-12-03 13:07:50 -0800

    analog and serail functionality

---
+++ STM32-F4/ADC/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.d
@@ -0,0 +1,86 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Inc/mxconstants.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Inc/mxconstants.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/Ro-ee/Documents/STM32Projects/ADC/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
