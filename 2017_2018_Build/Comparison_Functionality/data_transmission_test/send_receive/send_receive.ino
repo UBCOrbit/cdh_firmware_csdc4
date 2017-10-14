b4f621ffcb275f9a4e84305c2ed63ef4947d6a33
ctyfang <ctyfang@gmail.com>
2017-10-14 13:19:51 -0700

    Rename 2017_2018 Build to 2017_2018_Build. Implement test functions for STM and Arduino data communication via uart1.

---
+++ 2017_2018_Build/Comparison_Functionality/data_transmission_test/send_receive/send_receive.ino
@@ -0,0 +1,32 @@
+void setup() {
+  //arduino console
+  Serial.begin(115200);
+  //huart 1
+  Serial1.begin(115200);
+//  huart 6
+  Serial2.begin(115200);
+
+  //Board 1B
+  /*
+  pinMode(18,INPUT);
+  //Board 1C
+  pinMode(19,INPUT);
+  //Board 2B
+  pinMode(38,INPUT);
+  //Board 2C
+  pinMode(42,INPUT);
+  */
+}
+
+void loop() {
+
+  Serial1.write("AAHeelloooooHeelloooooHeelloooooHeelloooHeelloooooHeellooYZ");
+  //Serial3.print("AASuck my toes!YZ");
+
+  while(Serial1.available()>0){
+    Serial.println(Serial1.readString());
+  }
+}
+
+
+
