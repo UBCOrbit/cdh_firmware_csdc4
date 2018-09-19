1b6968086ddc9d52506ef0ce3ef6916757de1621
Andrada Zoltan <andrada.12321@gmail.com>
2018-09-18 22:42:52 -0700

    Final organization

---
+++ csdc_4/csdc_final_demo/comms_firmware/Debug/Src/lfsr.d
@@ -0,0 +1,7 @@
+Src/lfsr.o: ../Src/lfsr.c \
+ C:/Users/Andrada\ Zoltan/Desktop/UBCOrbit/CDH/csdc_4/csdc_final_demo/comms_firmware/Inc/lfsr.h \
+ C:/Users/Andrada\ Zoltan/Desktop/UBCOrbit/CDH/csdc_4/csdc_final_demo/comms_firmware/Inc/utils.h
+
+C:/Users/Andrada\ Zoltan/Desktop/UBCOrbit/CDH/csdc_4/csdc_final_demo/comms_firmware/Inc/lfsr.h:
+
+C:/Users/Andrada\ Zoltan/Desktop/UBCOrbit/CDH/csdc_4/csdc_final_demo/comms_firmware/Inc/utils.h:
